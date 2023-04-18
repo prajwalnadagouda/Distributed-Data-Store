c58f0be167e4f2a12dfb321965da2ff2918f42d5
Prajwal Nadagouda <32608138+prajwalnadagouda@users.noreply.github.com>
2023-04-18 02:06:47 -0700

    etl update

--- ETL/split.py
+++ ETL/split.py
@@ -1,14 +1,14 @@
 import pandas as pd
 source_path = "Parking_Violations_Issued_-_Fiscal_Year_2014.csv"
 
-
 df = pd.read_csv(source_path)
 cols = df.columns
-print(cols)
+
 for i in set(df['Issue Date']): # for classified by years files
-    i=i.split("/")
-    i=i[2]+"-"+i[0]+"-"+i[1]
+    j=i
+    j=j.split("/")
+    j=j[2]+"-"+j[0]+"-"+j[1]
     # i=str(i).replace("/","-")
-    filename = "main/"+i+".csv"
+    filename = "main/"+j+".csv"
     print(filename)
     df.loc[df['Issue Date'] == i].to_csv(filename,index=False,columns=cols)
